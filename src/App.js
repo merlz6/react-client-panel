0f7b76b5074a94e1a498cfe7238f94aea51af3f2
Michael Merlo <michaelmerlo@Michaels-MBP.home>
2019-05-05 14:11:09 -0400

    firebase connected, rendering clients and balance / total

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './store'
 import AppNavbar from './components/layout/AppNavbar';
+import Dashboard from './components/layout/Dashboard';
 import './App.css';
 
 function App() {
   return (
+    <Provider store={store}>
     <Router>
       <div className="App">
         <AppNavbar />
-        <h1>Hello</h1>
+        <div className="container">
+          <Switch>
+            <Route exact path ="/" component={Dashboard} />
+          </Switch>
+        </div>
       </div>
     </Router>
+    </Provider >
   );
 }
 
